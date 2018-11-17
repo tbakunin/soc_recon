61a4e7520dd5276120bc9fb57b077182ce3c1473
tbakunin <bakunin.t@protonmail.com>
2018-11-17 18:59:58 +0300

    Completed auth function

--- auth.py
+++ auth.py
@@ -1,33 +1,44 @@
-# done, tested
-
 import vk_api
+import os
 
 
-def auth():
-
+def auth(check_saved=1):
     def tfa_handler():
         code = input("[!] 2FA detected! Please enter code you've just received: ")
         return code, 0
 
-    while True:
-        print("[?] Do you want to authorize using login/pass or token?")
-        des = input("[?] P/ass/word for login/pass, T/oken for token auth: ")
-        if des.startswith("P"):
-            session = vk_api.VkApi(login=input("[?] Enter your login: "),
-                                   password=input("[?] Enter you password: "),
-                                   auth_handler=tfa_handler)
-            session.auth()
-            vk = session.get_api()
-            if vk.users.get(user_ids='1')[0]['id'] == 1:
-                return vk
-            else:
-                input("[-] Some error occurred, try again or Ctrl-C")
+    def inner_auth(login=None, password=None, tfa=tfa_handler):
+        session = vk_api.VkApi(login=login,
+                               password=password,
+                               auth_handler=tfa)
 
-        elif des.startswith("T"):
-            session = vk_api.VkApi(token=input("[?] Enter your token: "))
-            session.auth()
-            vk = session.get_api()
-            if vk.users.get(user_ids='1')[0]['id'] == 1:
-                return vk
+        session.auth()
+        vk = session.get_api()
+        if vk.users.get(user_ids='1')[0]['id'] == 1:
+            return vk
+        else:
+            print("[-] Some error occurred, try again or Ctrl-C")
+            return -1
+
+    if "creds" in os.listdir("resources") and check_saved:
+        with open("resources/creds.log", "r") as f:
+            lines = f.readlines()
+            if len(lines) == 2:
+                res = inner_auth(login=lines[0], password=lines[1])
             else:
-                input("[-] Some error occurred, try again (Enter) or exit (Ctrl-C)")
+                print("[!] Auth using saved creds failed, login manually!")
+                return auth(0)
+
+            if res == -1:
+                print("[!] Auth using saved creds failed, login manually!")
+                auth(0)
+            else:
+                return res
+    else:
+        login = input("[?] Enter your login: ")
+        password = input("[?] Enter you password: ")
+        if input("Do you want to save your creds? They WONT be encrypted [Y/es, N/o] ").upper().startswith("Y"):
+            with open("resources/creds.log", "w") as f:
+                f.write("\n".join((login, password)))
+        print("[+] Auth succeeded!")
+        return inner_auth(login=login, password=password)
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import vk_api
 from auth import auth
 from vk_extractor import extractor
+import sys
 
 session = auth()
 
-target = input("[?] Enter your target's ID: ")
 while True:
     print("""
           [i] Your options:
-          [1] Get general info
+          [1] Get general info about user
           [2] Find hidden friends
           [3] Dig info
           [4] Test whether account is fake
@@ -18,15 +18,18 @@ while True:
     des = input("[?] Choose 1-6: ")
     if not (len(des) == 1) or (49 > ord(des) or 54 < ord(des)):
         print("[!] Choose one of the options 1-6!")
+        continue
     else:
         pass
 
     if des == '1':
-        print(extractor(session, target))
+        print(extractor(session=session, target=input("[?] Enter your target's ID: ")))
         if input("[?] Run another module? [Y/n]").upper().startswith("Y"):
             continue
         else:
             break
+    elif des == '6':
+        sys.exit()
     else:
         print("[-] Not yet implemented")
         continue
